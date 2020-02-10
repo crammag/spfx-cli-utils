cb230e8374286b72e819913f28fbf63035c57d02
mark <arga.marc.01@gmail.com>
2020-02-10 02:34:15 +0100

    started working on config parsing and some dependencies added

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
 
-// Read config/config.json file
-    // Loop through the bundles object and per each key on the object, loop through the components array
-        // save the manifests file of each component
 
-    // Read all the manifests and load it on memory (they are simple JSON files, shouldn't be a problem)
-    // Apply the requested action to the manifests.
-    // Overwrite the manifest files if needed (check some way to preserve the comments) (but not strictily necessary)
-    // Save extra data to some folder like spfx-cli-data or something similar
-
-// Planned cli options
-    // - action (enum of supported actions)
-    // - fileName (name of the file to write to or to read from) (maybe separate into two parameters)
-    // - createBackup (for destructive actions, create a backup if this option is supplied)
-    // - path (relative path where we need to run the cli actions) (if not supplied, current folder is used instead)
+import * as fs from 'fs';
+import * as CJSON from 'comment-json';
+import * as argv from 'argv';
 
 
-// Currently planned actions
-    // - Randomize manifest ids (with an option to create a backup of the old ids)
-    // - Create a backup of the current ids
-    // - Restore ids from backup file created by this same tool
+// TODO: implement the reading of cli options, and use path instead or if not supplied, current path
+let configRaw = fs.readFileSync('spfx-project/config/config.json');
+
+// TODO: create an entity with the config schema (only needed properties);
+let config: any;
+
+try {
+    config = CJSON.parse(configRaw.toString('utf8'));
+    console.log(Object.keys(config.bundles));
+} catch(ex) {
+    console.error(ex);
+}
+
 
